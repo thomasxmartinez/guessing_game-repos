8767e6c95c94ef90b33944ad4ec65dc4fe34bbfc
Thomas Martinez <tommytnez@Thomass-MacBook-Air.local>
2016-09-15 21:16:12 -0700

    commit for 2nd lab, need to commit more often

---
+++ app.js
@@ -0,0 +1,55 @@
+
+var userName = prompt ('What\'s your name?');
+alert('Welcome to my site' + ' ' + userName);
+
+var answerOne = prompt('Was I under 10 years old when I first moved here?').toLowerCase();
+
+if (answerOne === 'y' || answerOne === 'yes') {
+  alert('You know me so well');
+} else {
+  alert('stranger danger');
+}
+
+var answerTwo = prompt('Was I was ever in Qatar?').toLowerCase();
+
+if (answerTwo === 'yes' || answerTwo === 'y') {
+  alert('Nice');
+} else {
+  alert('I would read that again');
+}
+
+var answerThree = prompt('Was I in Fl for more than two years?').toLowerCase();
+
+if (answerThree === 'y' || answerThree === 'yes') {
+  alert('you did it! you can read');
+} else {
+  alert('this is about me, not about anyone else');
+}
+
+var answerFour = prompt('Was I born a Mammal?').toLowerCase();
+
+if (answerFour === 'yes' || answerFour === 'y') {
+  alert('Yes yes yes');
+} else if (answerFour === 'no' || questionFour === 'n') {
+  alert('So you think I am cold blooded?');
+} else {
+  alert('please reply with yes,no,Y or N');
+}
+
+var answerFive = prompt('Was I born in WA?').toLowerCase();
+
+if (answerFive === 'yes' || answerFive === 'y') {
+  alert('You should probably read again ');
+} else if (answerFive === 'no' || answerFive === 'n') {
+  alert('Nice!');
+} else {
+  alert('please reply with yes,no,Y or N');
+}
+
+var answerSix = prompt('Name a location I have mentioned').toLowerCase();
+var myArray = ['washington','florida','qatar','los angeles','arizona'];
+if (myArray.indexOf(answerSix) === -1) {
+  alert('You seem to be losing the guessing game');
+} else {
+  alert('winner winner chicken dinner');
+}
